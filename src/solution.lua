57913594a9de0d22da8b23be3db9045206bf9c78
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2022-05-11 19:22:09 -0700

    Implement solution

--- src/solution.lua
+++ src/solution.lua
@@ -1,7 +1,13 @@
 local solution = {}
 
 function solution.solution(value)
-  return 23
+  local total = 0
+  for i=1,value-1 do
+    if ((i % 3 == 0) or (i % 5 == 0)) then
+      total = total + i
+    end
+  end
+  return total
 end
 
 return solution
--- tests/test.lua
+++ tests/test.lua
@@ -3,6 +3,7 @@ local solution = require 'solution'
 describe("Sample Test Cases", function()
   it("tests", function()
     assert.are.same(23, solution.solution(10))
+    assert.are.same(8, solution.solution(6))
   end)
 end)
 
