d5fb7430a33b50b89754d45ad81153523a8f2f99
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2022-05-11 19:57:06 -0700

    Add failing test

--- src/solution.lua
+++ src/solution.lua
@@ -1,6 +1,10 @@
 local solution = {}
 
-function solution.solution(value)
+function solution.even_or_odd(number)
+  -- Your code here
+end
+
+function solution.multiples_of_3_or_5(value)
   local total = 0
   for i=1,value-1 do
     if ((i % 3 == 0) or (i % 5 == 0)) then
--- tests/test.lua
+++ tests/test.lua
@@ -1,9 +1,19 @@
 local solution = require 'solution'
 
-describe("Sample Test Cases", function()
+describe('Even or Odd', function()
+  it('works for fixed tests', function()
+    assert.are.equal('Odd', solution.even_or_odd(1))
+    assert.are.equal('Even', solution.even_or_odd(2))
+    assert.are.equal('Odd', solution.even_or_odd(-1))
+    assert.are.equal('Even', solution.even_or_odd(-2))
+    assert.are.equal('Even', solution.even_or_odd(0))
+  end)
+end)
+
+describe("Multiples of 3 or 5", function()
   it("tests", function()
-    assert.are.same(23, solution.solution(10))
-    assert.are.same(8, solution.solution(6))
+    assert.are.same(23, solution.multiples_of_3_or_5(10))
+    assert.are.same(8, solution.multiples_of_3_or_5(6))
   end)
 end)
 
