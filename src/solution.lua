28fe66bd4e565a2ac0112b4b907681f4b8de21d0
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2022-05-15 17:55:03 -0700

    Format with LSP

--- src/solution.lua
+++ src/solution.lua
@@ -4,20 +4,18 @@ function kata.disemvowel(s)
     return s:gsub("[AEIOUaeiou]", "")
 end
 
-
 function kata.even_or_odd(number)
-  return number % 2 == 0 and "Even" or "Odd"
+    return number % 2 == 0 and "Even" or "Odd"
 end
 
-
 function kata.multiples_of_3_or_5(value)
-  local total = 0
-  for i=1,value-1 do
-    if ((i % 3 == 0) or (i % 5 == 0)) then
-      total = total + i
+    local total = 0
+    for i = 1, value - 1 do
+        if ((i % 3 == 0) or (i % 5 == 0)) then
+            total = total + i
+        end
     end
-  end
-  return total
+    return total
 end
 
 return kata
