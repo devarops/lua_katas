1fddfb798acb78eac8b1d19d24ee56b6db4e7ba0
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2022-05-17 17:32:09 -0700

    Format

--- tests/test.lua
+++ tests/test.lua
@@ -2,9 +2,9 @@ local solution = require("solution")
 
 describe("Find The Parity Outlier", function()
     it([[You are given an array (which will have a length of at least 3, but could be very large)
-  containing integers. The array is either entirely comprised of odd integers or entirely comprised
-  of even integers except for a single integer N. Write a method that takes the array as any
-  argument and returns this "outlier" N.]], function()
+    containing integers. The array is either entirely comprised of odd integers or entirely
+    comprised of even integers except for a single integer N. Write a method that takes the array
+    as any argument and returns this "outlier" N.]], function()
         assert.are.same(3, solution.FindOutlier.find({ 2, 6, 8, -10, 3 }))
         assert.are.same(206847684, solution.FindOutlier.find({ 206847684, 1056521, 7, 17, 1901, 21104421, 7, 1, 35521, 1, 7781 }))
         assert.are.same(0, solution.FindOutlier.find({ 2147483647, 0, 1 }))
