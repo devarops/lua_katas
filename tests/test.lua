c45bf1e4a32073282d4c6c2bb5c224d4af0a598b
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2023-05-20 04:31:08 +0000

    🛑🧪 Fail tests

--- tests/test.lua
+++ tests/test.lua
@@ -1,6 +1,27 @@
 -- https://www.codewars.com/kata/search/lua?q=&xids=completed&beta=false&order_by=popularity%20desc
-
 local solution = require("solution")
+
+--[[ Stop gninnipS My sdroW!
+https://www.codewars.com/kata/5264d2b162488dc400000001/train/lua
+
+Write a function that takes in a string of one or more words, and returns the same string, but with
+all five or more letter words reversed (Just like the name of this Kata). Strings passed in will
+consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+Examples:
+
+```
+spinWords( "Hey fellow warriors" ) => returns "Hey wollef sroirraw"
+spinWords( "This is a test") => returns "This is a test"
+spinWords( "This is another test" )=> returns "This is rehtona test"
+```
+]]
+
+describe("TDD:", function()
+  it("The function exists", function()
+    assert(type(solution.spinWords) == "function", "spinWords() does not exist!")
+  end)
+end)
 
 --[[ Vowel Count
 https://www.codewars.com/kata/54ff3102c1bad923760001f3/train/lua
