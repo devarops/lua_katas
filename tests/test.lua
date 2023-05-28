f8d59c5706459c420da10eda01705069132391d2
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2023-05-28 18:52:34 +0000

    ♻️  Refactor. This is the way.

--- tests/test.lua
+++ tests/test.lua
@@ -301,12 +301,22 @@ describe("even_or_odd", function()
     end)
 end)
 
-describe("Multiples of 3 or 5", function()
-    it([[If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6
-    and 9. The sum of these multiples is 23. Finish the solution so that it returns the sum of all
-    the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative,
-    return 0 (for languages that do have them).
-    Note: If the number is a multiple of both 3 and 5, only count it once.]], function()
+--[[ Multiples of 3 or 5
+https://www.codewars.com/kata/514b92a657cdc65150000006/train/lua
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The
+sum of these multiples is 23.
+
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number
+passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
+
+Note: If the number is a multiple of both 3 and 5, only count it once.
+
+Courtesy of projecteuler.net ([Problem 1](https://projecteuler.net/problem=1))
+]]
+
+describe("multiples", function()
+    it("should return the sum of all multiples of 3 or 5 below the number passed in", function()
         assert.are.same(23, solution.multiples(10))
         assert.are.same(8, solution.multiples(6))
     end)
