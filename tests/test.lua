f43e43308a5ad9e91d2770cbb7d374c9cf0df522
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2023-05-28 18:38:42 +0000

    ♻️  Refactor. This is the way.

--- tests/test.lua
+++ tests/test.lua
@@ -218,14 +218,28 @@ describe("find", function()
     end)
 end)
 
-describe("Mumbling", function()
+--[[ Mumbling
+https://www.codewars.com/kata/5667e8f4e3f572a8f2000039/train/lua
+
+This time no story, no theory. The examples below show you how to write function accum:
+
+## Examples
+
+```
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+```
+
+The parameter of accum is a string which includes only letters from a..z and A..Z.
+]]
+
+describe("accum", function()
     local function dotest(s, expect)
         local actual = solution.accum(s)
         assert.are.same(expect, actual)
     end
-
-    it([[The parameter of accum is a string which includes only letters from a..z and A..Z.
-    ]], function()
+    it("basic tests", function()
         dotest("ZpglnRxqenU", "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu")
         dotest("NyffsGeyylB", "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb")
         dotest("MjtkuBovqrU", "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu")
