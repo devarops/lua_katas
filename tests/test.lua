07e07f114fa0ca77fb0f5814c745bad13badd1c2
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2023-06-05 00:32:12 +0000

    ✅ Pass tests. This is the way.

--- tests/test.lua
+++ tests/test.lua
@@ -25,32 +25,11 @@ describe("count_bits", function()
   it("should return 1 for 2", function()
     assert.are.same(solution.count_bits(2), 1)
   end)
---[[
-  it("should return 3 for 3", function()
-    assert.are.same(solution.count_bits(3), 2)
-  end)
-  it("should return 4 for 4", function()
-    assert.are.same(solution.count_bits(4), 1)
-  end)
-  it("should return 5 for 5", function()
-    assert.are.same(solution.count_bits(5), 2)
-  end)
-  it("should return 6 for 6", function()
-    assert.are.same(solution.count_bits(6), 2)
-  end)
-  it("should return 7 for 7", function()
-    assert.are.same(solution.count_bits(7), 3)
-  end)
-  it("should return 8 for 8", function()
-    assert.are.same(solution.count_bits(8), 1)
-  end)
-  it("should return 9 for 9", function()
-    assert.are.same(solution.count_bits(9), 2)
-  end)
-  it("should return 10 for 10", function()
-    assert.are.same(solution.count_bits(10), 2)
-  end)
-]]
+  local input = {0,4,7,9,10,26,77231418,12525589,3811,392902058,1044,10030245,183337941,20478766,103021,287,115370965,31,417862,626031,89,674259}
+  local expected = {0,1,3,2,2,3,14,11,8,17,3,10,16,14,9,6,15,5,7,12,4,10}
+  for i = 1, #input do
+    it("Basic test " .. i, function() assert.are.same(expected[i], solution.count_bits(input[i])) end)
+  end
 end)
 
 --[[ Highest and Lowest
