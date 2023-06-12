d87dd8652486fbb59cc23c79e54d49c99b0844e9
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2023-06-12 21:33:51 +0000

    🛑🧪 Fail tests. This is the way.

--- tests/test.lua
+++ tests/test.lua
@@ -1,6 +1,46 @@
 -- https://www.codewars.com/kata/search/lua?q=&xids=completed&beta=false&order_by=popularity%20desc
 local solution = require("solution")
 
+--[[ You're a square!
+https://www.codewars.com/kata/54c27a33fb7da0db0100040e/train/lua
+
+# A square of squares
+
+You like building blocks. You especially like building blocks that are squares. And what you even
+like more, is to arrange them into a square of square building blocks!
+
+However, sometimes, you can't arrange them into a square. Instead, you end up with an ordinary
+rectangle! Those blasted things! If you just had a way to know, whether you're currently working in
+vain… Wait! That's it! You just have to check if your number of building blocks is a perfect square.
+
+## Task
+
+Given an integral number, determine if it's a [square
+number](https://en.wikipedia.org/wiki/Square_number):
+
+> In mathematics, a _square number_ or _perfect square_ is an integer that is the square of an
+integer; in other words, it is the product of some integer with itself.
+
+The tests will always use some integral number, so don't worry about that in dynamic typed
+languages.
+
+## Examples
+```
+-1  =>  false
+ 0  =>  true
+ 3  =>  false
+ 4  =>  true
+25  =>  true
+26  =>  false
+```
+]]
+
+describe("issquare", function()
+  it("The function exists", function()
+    assert(type(solution.issquare) == "function", "issquare() does not exist!")
+  end)
+end)
+
 --[[ Bit Counting
 https://www.codewars.com/kata/526571aae218b8ee490006f4/train/lua
 
