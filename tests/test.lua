bbda832e55885930b63871f32ad5474a40564447
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2023-06-19 22:18:18 +0000

    🛑🧪 Fail tests. This is the way.

--- tests/test.lua
+++ tests/test.lua
@@ -1,6 +1,29 @@
 -- https://www.codewars.com/kata/search/lua?q=&xids=completed&beta=false&order_by=popularity%20desc
 local solution = require("solution")
 
+--[[ Take a Ten Minutes Walk
+https://www.codewars.com/kata/54da539698b8a2ad76000228/train/lua
+
+You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten
+minutes too early to an appointment, so you decided to take the opportunity to go for a short walk.
+The city provides its citizens with a Walk Generating App on their phones -- everytime you press the
+button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's',
+'w', 'e']). You always walk only a single block for each letter (direction) and you know it takes
+you one minute to traverse one city block, so create a function that will return true if the walk
+the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will,
+of course, return you to your starting point. Return false otherwise.
+
+> Note: you will always receive a valid array containing a random assortment of direction letters
+('n', 's', 'e', or 'w' only). It will never give you an empty array (that's not a walk, that's
+standing still!).
+]]
+
+describe("is_valid_walk", function()
+  it("The function exists", function()
+    assert(type(solution.is_valid_walk) == "function", "is_valid_walk() does not exist!")
+  end)
+end)
+
 --[[ You're a square!
 https://www.codewars.com/kata/54c27a33fb7da0db0100040e/train/lua
 
